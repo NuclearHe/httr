f4505086895d00c6ef7881e9ccb02e8d591d2b48
NuclearHe <1787414308@qq.com>
2020-04-08 14:24:44 +0800

    Update http-post.r

--- R/http-post.r
+++ R/http-post.r
@@ -51,7 +51,7 @@ POST <- function(url = NULL, config = list(), ..., body = NULL,
                  encode = c("multipart", "form", "json", "raw"),
                  handle = NULL) {
   encode <- match.arg(encode)
-
+print(url)
   hu <- handle_url(handle, url, ...)
   req <- request_build("POST", hu$url, body_config(body, match.arg(encode)), as.request(config), ...)
   request_perform(req, hu$handle$handle)
